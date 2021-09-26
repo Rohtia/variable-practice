adbf8cefc965400ac3c42ca591f96edbe7da94ce
Rohtia <90939780+Rohtia@users.noreply.github.com>
2021-09-26 11:06:51 -0400

    Object variables added

--- model-1.js
+++ model-1.js
@@ -13,3 +13,15 @@ var dinnerReady = false;
 var makeCoffee = ['water', 'coffee grounds', 'pinch of salt', 'cinnamon'];
 var makeLunch = ['bread', 'tuna', 'mayo'];
 var makeDinner = ['ground meat', 'corn', 'diced tomatoes', 'beans', 'spices'];
+
+class Menu {
+  constructor(day, meal, ing) {
+    this.day = day;
+    this.meal = meal;
+    this.ingredients = ing;
+  }
+}
+
+var meal1 = new Menu ('Monday', 'dinner', ['chicken', 'rice', 'corn']);
+var meal2 = new Menu ('Tuesday', 'lunch', ['bread', 'chicken', 'mayo']);
+var meal3 = new Menu ('Tuesday', 'dinner', [makeDinner]);
