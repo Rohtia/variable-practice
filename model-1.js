138a15d11d456c2b7b1ac2321ece510bd8d1fdc3
Rohtia <90939780+Rohtia@users.noreply.github.com>
2021-09-26 10:58:23 -0400

    Number variables added

--- model-1.js
+++ model-1.js
@@ -1,3 +1,7 @@
 var morning = "It is morning. I need coffee.";
 var lunch = "What should we have for lunch?";
 var dinner = "Dinner will be chili.";
+
+var cups = 4;
+var plates = 2;
+var bowls = 3;
